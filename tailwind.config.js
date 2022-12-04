204f3ff757c55c8d842ca04f70415a23e9d015f2
“bryson” <brysonward77@gmail.com>
2022-12-04 09:53:07 -0500

    added custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#4ADE80',
+        'main-bg': '#202020'
+      }
+    },
   },
   plugins: [],
 }
