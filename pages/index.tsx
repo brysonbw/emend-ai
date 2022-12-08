067dd23c95990a927f5d6c359dc12fb143c7ee58
“bryson” <brysonward77@gmail.com>
2022-12-08 13:49:56 -0500

    edit home page hero split styling/layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,26 @@ import { FormSchema } from '../lib/formSchema';
 
 export default function Home() {
   const onSubmit = (data: FormSchema) => {
-    console.log(data);
+    console.log(data); // display form data
   };
   return (
-    <section className="mt-auto">
-      <h1>Emend AI: A GPT3 Email Generator</h1>
-      {/** Form */}
-      <Form onSubmit={onSubmit} />
-    </section>
+    <main className="mt-auto">
+      <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+        {/** Left side hero */}
+
+        {/** Form */}
+        <section className="mx-auto text-center">
+          <Form onSubmit={onSubmit} />
+        </section>
+
+        {/** Right Side Hero */}
+        {/**
+         * // TODO: Display GPT response after successful onSubmit()
+         */}
+        <section className="mx-auto text-center">
+          <h1>Right Side</h1>
+        </section>
+      </div>
+    </main>
   );
 }
